8be4d84fb8f78a0b05fb1076ef5e85ad0159153c
Robert Schnüll <mail@robertschnuell.de>
2017-04-11 00:35:28 +0200

    motor controller : limit switch self leveling and fail safe added

--- code/Arduino/src/main.ino
+++ code/Arduino/src/main.ino
@@ -33,9 +33,10 @@ bool limit;
  unsigned long sendTimer = 0;
  int timeing = 50;
 
- boolean iniV  = false;
 
  double lastCurrent = 0;
+
+ boolean initialization = false;
 
 ////// Stepper library //////
 
@@ -51,7 +52,7 @@ byte mac[] = {
   0xDE, 0xAD, 0xBE, 0xEF, 0xFE, 0xED
 };
 IPAddress ip(192, 168, 0, 177);
-IPAddress server(192, 168, 0, 105);
+IPAddress server(192, 168, 0, 104);
 
 EthernetClient ethClient;
 PubSubClient client(ethClient);
@@ -63,7 +64,19 @@ unsigned long time;
 
 
 void setup() {
-  ini();
+
+  //ini
+  min  = -1800;
+  max = 400;
+  current = 0;
+  target = 0;
+  speed = 50;
+  status = false;
+  limit = false;
+  accel = 400;
+  maxSpeed = 800;
+
+
   //Serial.begin(9600);
   mot.begin(FULL_STEP, 200);
 
@@ -80,70 +93,103 @@ void setup() {
   delay(2500);
   printIPAddress();
 
+  //limit switch
+  pinMode(7,INPUT);
+
+
+  delay(5000);
+
 }
 
-void ini() {
-  min  = -1800;
-  max = 0;
-  current = 0;
-  target = 0;
-  speed = 100;
-  status = false;
-  limit = false;
-  accel = 400;
-  maxSpeed = 800;
-}
+
 
 double fmap (double sensorValue, double sensorMin, double sensorMax, double outMin, double outMax)
 {
   return (sensorValue - sensorMin) * (outMax - outMin) / (sensorMax - sensorMin) + outMin;
 }
 
+void ini() {
+  if(!limit) {
+    mot.moveTo(mot.forwardLimit);
+
+  }
+
+    initialization = true;
+}
 
 void loop() {
-  mot.work();
 
-  client.loop();
-
-
-
-  if (!client.connected())
-{
-    if ( client.connect("testusr") ) {
-      Serial.println("connected");
-    }
-    String tmp = "a0 online ";
-    tmp += random(0,1000);
-    char temp[tmp.length() + 1];
-    tmp.toCharArray(temp, tmp.length() + 1);
-
-  //  Serial.println(tmp);
-
-    client.publish("a0/main", temp);
-    client.subscribe("a0/target");
-    client.subscribe("a0/speed");
+  if(!digitalRead(7)) {
+      limit = true;
+  } else {
+      limit = false;
   }
 
 
+  if(!limit) {
+      mot.work();
+  } else {
+    mot.stop();
 
-  if( (sendTimer + timeing) < millis() ) {
+    mot.moveTo(mot.reverseLimit);
+    while(!digitalRead(7)) {
+      mot.work();
 
-    String tmp ;
-    double val = fmap(mot.getPos(),min,max,0.000,100.000);
+    }
+    mot.stop();
+    min = mot.getPos() - 50;
+    Serial.print("min is: ");
+    Serial.println(mot.getPos());
+    mot.moveTo(min);
+  }
 
-    if( lastCurrent != val) {
-      lastCurrent = val;
+  if(!initialization) {
+      ini();
+  } else {
+    client.loop();
 
-      tmp += millis();
-      tmp = val;
 
+
+    if (!client.connected()) {
+      if ( client.connect("testusr") ) {
+        Serial.println("connected");
+      }
+      String tmp = "a0 online ";
+      tmp += random(0,1000);
       char temp[tmp.length() + 1];
       tmp.toCharArray(temp, tmp.length() + 1);
-      client.publish("a0/current",temp);
-      sendTimer = millis();
+
+    //  Serial.println(tmp);
+
+      client.publish("a0/main", temp);
+      client.subscribe("a0/target");
+      client.subscribe("a0/speed");
+      client.subscribe("a0/main");
     }
 
-  }
+
+
+    if( (sendTimer + timeing) < millis() ) {
+
+      String tmp ;
+      double val = fmap(mot.getPos(),min,max,0.000,100.000);
+
+      if( lastCurrent != val) {
+        lastCurrent = val;
+
+        tmp += millis();
+        tmp = val;
+
+        char temp[tmp.length() + 1];
+        tmp.toCharArray(temp, tmp.length() + 1);
+        client.publish("a0/current",temp);
+        sendTimer = millis();
+      }
+
+    }
+
+
+  } // end if INI
 
 
 }
@@ -174,23 +220,33 @@ void callback(char* topic, byte* payload, unsigned int length) {
   if(lvl1.equals("a0")) {
     if(lvl2.equals("target")) {
 
-      if(!iniV) {
-        mot.moveTo(-200);
-        iniV = true;
-        //Serial.println("ini Done");
-        max = -200;
-      }  else {
+
 
         int newPos = int(fmap(msgString.toFloat(),0.00,100.00,min,max));
         mot.moveTo(newPos);
-      }
+
 
     } else if (lvl2.equals("speed")) {
       Serial.println("speed");
       speed = int(fmap(msgString.toFloat(),0.00,100.00,0,maxSpeed));
       Serial.println(speed);
       mot.setMaxSpeed(speed);
+
+    } else if(lvl2.equals("main")) {
+      Serial.println("main");
+      Serial.println(msgString);
+      if(  msgString.substring(0,msgString.indexOf(' ')).equals("ini-max")  ) {
+        Serial.print("max was:\t");
+        Serial.print(max);
+        max += (msgString.substring(msgString.indexOf(' ')+1)).toInt();
+
+        Serial.print("max is now:\t");
+        Serial.println(max);
+
+      } 
+
     }
+
   }
 
 
